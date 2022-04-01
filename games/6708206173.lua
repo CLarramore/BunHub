8dc06c185f429134dbddcb9b8c4506ba84deb9ef
CLarramore <cdlarramore@gmail.com>
2022-04-01 18:37:52 -0400

    Ben

--- games/6708206173.lua
+++ games/6708206173.lua
@@ -1,32 +1,27 @@
--- Made by Brickmane#0447 pls dont remove credit :(
+-- Made by Brickmane#0447
+-- fixed by swat turret#5653
 
 local Players = game:GetService("Players")
 
-
 function changeName(n)
-local A_1 = "Update"
-local A_2 = 
-{
-    ["DescriptionText"] = n, 
-    ["ImageId"] = nil
-}
-local event = game:GetService("ReplicatedStorage").CustomiseBooth
-event:FireServer(A_1, A_2)
+	local A_1 = "Update"
+	local A_2 = {
+		["DescriptionText"] = n,
+		["ImageId"] = nil,
+	}
+	local event = game:GetService("ReplicatedStorage").CustomiseBooth
+	event:FireServer(A_1, A_2)
 end
 
 function changeImage(n)
-local A_1 = "Update"
-local A_2 = 
-{
-    ["DescriptionText"] = nil, 
-    ["ImageId"] = n
-}
-local event = game:GetService("ReplicatedStorage").CustomiseBooth
-event:FireServer(A_1, A_2)
+	local A_1 = "Update"
+	local A_2 = {
+		["DescriptionText"] = nil,
+		["ImageId"] = n,
+	}
+	local event = game:GetService("ReplicatedStorage").CustomiseBooth
+	event:FireServer(A_1, A_2)
 end
-
-
-
 
 local function BenDialogue()
 	local Randomize = math.random(1, 4)
@@ -36,29 +31,24 @@ local function BenDialogue()
 		changeName("No.")
 	elseif Randomize == 3 then
 		changeName("HOHOHO")
-    elseif Randomize == 4 then
+	elseif Randomize == 4 then
 		changeName("Ough.")
-    end
+	end
 end
 
-
-
-
-changeName("Talking Ben \n (cant use mic)")
+changeName("Talking Ben")
 changeImage(8962954473)
 
-Players.PlayerChatted:Connect(function(plr, message) --When a player speaks
-    for _,plrs in pairs(game.Players:GetPlayers()) do
-	
-	if (Players.LocalPlayer.Character.HumanoidRootPart.Position - plrs.Character.HumanoidRootPart.Position).magnitude <= 12 then 
-		if plr.Name == plrs.Name then
-		local responses={"Yes.","No.","Hohoho","Ugh."}
-		    changeName(responses[math.random(1,#responses)])
-            print(message, plr) -- chat log
-            task.wait(4)
-            changeName("Talking Ben \n (cant use mic)") --The function is called
-		end
+Players.PlayerChatted:Connect(function(chatType, plr, message) --When a player speaks
+	if chatType ~= Enum.PlayerChatType.All then return end
+	if
+		(Players.LocalPlayer.Character.HumanoidRootPart.Position - plr.Character.HumanoidRootPart.Position).Magnitude
+		<= 12
+	then
+	local responses={"Yes.","No.","Hohoho","Ugh."}
+		changeName(responses[math.random(1,#responses)])
+		print(message, plr) -- chat log
+		task.wait(4)
+		changeName("Talking ben") --The function is called
 	end
-	
-end
 end)
