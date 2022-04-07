db98339ccfbc8aa2a6b3230434c214f51a5bdc55
CLarramore <cdlarramore@gmail.com>
2022-04-07 14:25:14 -0400

    Added PetName Response Toggle
    Added a feature to determine whether or not you want to use the pet name to respond to players.

--- games/1948436601.lua
+++ games/1948436601.lua
@@ -14,6 +14,9 @@ Enabled=true;
 Distance=20;
 NameVisible=true;
 chatfuel="False";};
+local settings={
+petname=true;
+};
 
 
 
@@ -26,13 +29,29 @@ Name=credit.Creator.Name;
 Description="Creator of this script.";
 Discord=credit.Creator.Discord;
 };
-if plr.Character:FindFirstChild("ModelPet_Bunny")==nil then
+if plr.Character:FindFirstChild("ModelPet_Bunny")==nil and settings.petname then
 events["Event_PetHandler"]:FireServer("Pet_Bunny",true)
 end
 _G.chatbotsettings.callchat=function(output)
+if settings.petname then
 events["Event_MakePetRPName"]:FireServer(output)
+else
+game:GetService("ReplicatedStorage")["DefaultChatSystemChatEvents"]["SayMessageRequest"]:FireServer(output, 'All')
+end
 end
 
 _G.gui=gui
 local Chatbot=loadstring(game:HttpGet("https://raw.githubusercontent.com/CLarramore/BunHub/main/scripts/chatbot.lua"))
 
+local GameChatbot=Chatbot:Section{
+Name="Game Exclusive";};
+
+GameChatbot:Toggle{
+Name="Use Pet Name";
+Description="Uses the pet name to respond for you (unfiltered)";
+StartingState=settings.petname;
+Callback=function(state) settings.petname=state;
+if settings.petname==true then
+events["Event_PetHandler"]:FireServer("Pet_Bunny",true) else
+events["Event_PetHandler"]:FireServer("Pet_Bunny",false)
+end end;};
