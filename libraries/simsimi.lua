61ae2db12fde0f08592a46cfb5b4505fbb687022
CLarramore <cdlarramore@gmail.com>
2022-04-07 14:12:31 -0400

    Added Name Feature

--- libraries/simsimi.lua
+++ libraries/simsimi.lua
@@ -6,10 +6,11 @@ local Mena={}
 local settings=settings or {}
 Mena.language=settings.langauge or "en"
 Mena.chatfuel=settings.chatfuel or "True"
+Mena.Name=settings.Name or "Ribbon"
 
 function Mena:response(message)
 local text=message
-    local Response = game:HttpGet("https://api.simsimi.net/v2/?text="..text.."&lc="..Mena.language.."&cf="..Mena.chatfuel)
+    local Response = game:HttpGet("https://api.simsimi.net/v2/?text="..text.."&lc="..Mena.language.."&cf="..Mena.chatfuel.."&name="..Mena.Name)
 	local data=HttpService:JSONDecode(Response)
 	return data.success
 	end
