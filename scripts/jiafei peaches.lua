b9e96e3eca9bb07aae63d424a3f7422e8da6bb2b
CLarramore <cdlarramore@gmail.com>
2022-04-13 11:50:13 -0400

    Chatbot wont work
    OOF

--- scripts/jiafei peaches.lua
+++ scripts/jiafei peaches.lua
@@ -6,6 +6,10 @@ be near fat fag in that jail cell for it to work. should kill everyone but you.
 i will optimize it later on. 
 ]]--
 local plr=game.Players.LocalPlayer
+ch=plr.Character
+_G.oldCF=ch.HumanoidRootPart.CFrame
+ch.HumanoidRootPart.Anchored=true
+ch.HumanoidRootPart.CFrame=workspace.Peaches.HumanoidRootPart.CFrame--*CFrame.new(0,-5,0)
 for a,b in pairs(game.Players:GetPlayers()) do
 if b.UserId~=plr.UserId then
 if b.Character~=nil then
@@ -13,12 +17,13 @@ local char=b.Character
 if char:FindFirstChild("HumanoidRootPart")~=nil then
 
 char.HumanoidRootPart.Anchored=true
-char.HumanoidRootPart.CFrame=workspace.Peaches.UpperTorso.CFrame*CFrame.new(0,0,0)*CFrame.Angles(0,math.pi/2,0)--math.random(-20,20)/10,0,math.random(-20,20)/10)
+char.HumanoidRootPart.CFrame=workspace.Peaches.LowerTorso.CFrame*CFrame.new(0,0,0)--*CFrame.Angles(0,math.pi/2,0)--math.random(-20,20)/10,0,math.random(-20,20)/10)
 for c,d in pairs(char:GetDescendants()) do if d:IsA("BasePart") then d.CanCollide=false end end
 --firetouchinterest(char.HumanoidRootPart,game.Workspace.Peaches.UpperTorso,1)
 end end end end
-
-
+ch.HumanoidRootPart.Anchored=false
+--ch.HumanoidRootPart.CFrame=_G.oldCF
+--_G.oldCF=nil
 --[[
 
 for c,d in pairs(char:GetDescendants()) do if d:IsA("BasePart") then d.CanCollide=true end end
