88dbc606854309c74d7077d6e923dd60060606b4
CLarramore <cdlarramore@gmail.com>
2022-07-29 19:58:10 -0400

    Update log.lua

--- auto/log.lua
+++ auto/log.lua
@@ -1,9 +1,37 @@
-local WEBHOOK = readfile("url.txt")
+--[==[HELP]==
+[1] - string | nil
+	The webhook URL.
+
+[2] - string | boolean | nil
+	If set to true or not passed in, writes to a file to path formatted as "./logs/%011(placeId) %Y-%m-%d %H%M%S.txt".
+]==] --
+--
+local DEFAULT_WEBHOOK = nil
+
 request=request or syn.request
+local args = _G.EXEC_ARGS or {}
+local WEBHOOK = readfile("url.txt")
+local FILEPATH = true
+
+if WEBHOOK == nil then WEBHOOK = DEFAULT_WEBHOOK end
+--[[
+if WEBHOOK then
+	if WEBHOOK:find '^https://discorda?p*%.com/api/webhooks/' then
+		WEBHOOK = WEBHOOK:sub(-87)
+	end
+	WEBHOOK = 'https://hooks.hyra.io/api/webhooks/' .. WEBHOOK
+end
+]]--
 local pId = game.PlaceId
 local sId = #game.JobId > 0 and game.JobId or 'PLAYTEST'
 local uId = game.Players.LocalPlayer.UserId
 local enabled = true
+
+local function timestamp(t) return os.date('%Y-%m-%dT%H:%M:%SZ', t) end
+if type(FILEPATH) ~= 'string' and FILEPATH ~= false then
+	FILEPATH = string.format('logs/%011d %s.txt', pId, os.date('%Y-%m-%d %H%M%S'))
+	makefolder('logs')
+end
 
 local function get_pname(pId)
 	local s, r = pcall(
@@ -25,20 +53,12 @@ if not pName then
 		end)
 end
 
-local function timestamp(t) return os.date('%Y-%m-%dT%H:%M:%SZ', t) end
-local fn = string.format('logs/%011d %s', pId, os.date('%Y-%m-%d %H%M%S.txt'))
-makefolder('logs')
-
-if WEBHOOK:find '^https://discorda?p*%.com/api/webhooks/' then
-	WEBHOOK = WEBHOOK:sub(-87)
-end
-WEBHOOK = 'https://discord.com/api/webhooks/' .. WEBHOOK
-
 function header(t)
 	return string.format('[%11s] : [%11s] %s - %s', uId, pId, pName, sId)
 end
 
 function disc_send(txt)
+	if not WEBHOOK then return end
 	while not request do wait(1) end
 	return request{
 		Url = WEBHOOK,
@@ -56,7 +76,7 @@ function _G.dlog(ln, frc, ts)
 	ln = string.format('%s: %s', timestamp(ts), ln)
 	_G.dlog_snip = _G.dlog_snip .. '\n' .. ln
 	_G.dlog_long = _G.dlog_long .. '\n' .. ln
-	writefile(fn, _G.dlog_long)
+	if FILEPATH then writefile(FILEPATH, _G.dlog_long) end
 	local t = tick()
 	last = t
 
@@ -114,11 +134,13 @@ if _G.dlog_evts then for _, e in next, _G.dlog_evts do e:Disconnect() end end
 _G.dlog_evts = {
 	game.Players.PlayerAdded:Connect(player_add),
 	game.Players.PlayerRemoving:Connect(player_lve),
+	--[[
 	game.Close:Connect(
 		function()
 			_G.dlog('CHAT STREAM SUCCESSFULLY ENDED', true)
 			enabled = false
 		end),
+		]]
 }
 
 _G.dlog_snip = header()
